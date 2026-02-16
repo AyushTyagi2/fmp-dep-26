1a24da6f576d4602ed14a850ee9479c07b8aa10a
Nishant_Sahni <nishantsahni04@gmail.com>
2026-02-16 22:53:23 +0530

    made inserts for db population and also fixed some db issues

--- src/FmpBackend/obj/Debug/net8.0/FmpBackend.AssemblyInfo.cs
+++ src/FmpBackend/obj/Debug/net8.0/FmpBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FmpBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec3eb5c9f70ec039b5c1b2f666673363ea78c4a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f31ce6767ad9fe7299cf99475d22a43e4ecc0c48")]
 [assembly: System.Reflection.AssemblyProductAttribute("FmpBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FmpBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
