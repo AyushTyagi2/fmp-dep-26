72b1f68e5dc263369ab561e8667d5b62284759e5
Nishant_Sahni <nishantsahni04@gmail.com>
2026-03-18 03:53:00 +0530

    merge

--- src/FmpBackend/obj/Debug/net8.0/FmpBackend.AssemblyInfo.cs
+++ src/FmpBackend/obj/Debug/net8.0/FmpBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FmpBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ecdcdaa893b77a2360aedb35b00b3fc8d17f64d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+706d04f0db85b6bd23e1820f08d1af7daa15fbf7")]
 [assembly: System.Reflection.AssemblyProductAttribute("FmpBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FmpBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
