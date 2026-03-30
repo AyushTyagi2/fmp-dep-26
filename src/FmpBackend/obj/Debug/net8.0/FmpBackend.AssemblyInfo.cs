019df3d3838e8f86c49be6c83de261efb61c03c6
dkv1234 <dkv3217@gmail.com>
2026-03-30 19:24:44 +0530

    sender home added

--- src/FmpBackend/obj/Debug/net8.0/FmpBackend.AssemblyInfo.cs
+++ src/FmpBackend/obj/Debug/net8.0/FmpBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FmpBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2865e97da42ca2fd2575368ad99b3798178b6563")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7c6de7a4668e5080ff169c35d114d653580e2b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("FmpBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FmpBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
